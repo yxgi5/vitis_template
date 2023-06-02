76e1987f859328716ebffcf6dbae6192866982a1
Andreas Zhang <denglitsch@gmail.com>
2023-06-02 14:46:17 +0800

    fix:

--- vivado/compare_src.bat
+++ vivado/compare_src.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=C:\Program Files\Beyond Compare 4;%PATH%
 
-bcompare vivado\vivado_proj\system.tcl vivado\bd\system.tcl
-bcompare vivado\vivado_proj\vivado_proj.srcs\constrs_1\imports\xdc\system.xdc vivado\xdc\system.xdc
+bcompare vivado_proj\system.tcl bd\system.tcl
+bcompare vivado_proj\vivado_proj.srcs\constrs_1\imports\xdc\system.xdc xdc\system.xdc
--- vivado/compare_src.sh
+++ vivado/compare_src.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bcompare vivado/vivado_proj/system.tcl vivado/bd/system.tcl
-bcompare vivado/vivado_proj/vivado_proj.srcs/constrs_1/imports/xdc/system.xdc vivado/xdc/system.xdc
+bcompare vivado_proj/system.tcl bd/system.tcl
+bcompare vivado_proj/vivado_proj.srcs/constrs_1/imports/xdc/system.xdc xdc/system.xdc
