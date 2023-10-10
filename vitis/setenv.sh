9935836b957fd1e37ef91933bf241515c89365a7
Andreas Zhang <denglitsch@gmail.com>
2023-10-10 19:27:10 +0800

    update: vitis bash script, unfortunately cannot use bash xsct on windows

--- vitis/setenv.sh
+++ vitis/setenv.sh
@@ -1,9 +1,118 @@
 #!/bin/bash
 
-echo -e "\033[42;31mScript ver 0.1.001 \nMake sure the env path is correct before running!! \033[0m"
+## assume you have Vivado 2020.1 (and Vitis) installed in D:\Xilinx\, which is necessary
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    source /opt/Xilinx/Vitis/2020.1/settings64.sh
+# you need one of bash.exe from following way
+## assume you have msys64 installed in D:\msys64\, offering bash.exe
+## assume you have Git installed in C:\Program Files\, offering bash.exe(not git-bash.exe)
+## assume you have cygwin64 installed in D:\cygwin64\, offering bash.exe
+
+# you can get OSTYPE by command `bash -c "echo $OSTYPE"`
+# here is an avilable list and coresponding OS
+# darwin*       # Mac OS
+# darwin9       # iOS
+# solaris*      # Solaris
+# linux-gnu     # Android
+# linux-android # Android (termux) 
+# linux-gnu     # Linux
+# cygwin        # CYGWIN
+# msys          # MSYS2
+# linux-gnu     # Win10 bash
+# openbsd*      # OpenBSD
+# FreeBSD       # FreeBSD
+# netbsd        # NetBSD
+# ...
+
+
+# open bash.exe and 
+# DIR=`cygpath -ua "D:\path\to\project"` && cd $DIR
+# source setenv.sh
+
+query_vars_msys() {
+  (cmd.exe /c "call D:\Xilinx\Vitis\2020.1\settings64.bat && D:\Xilinx\Vivado\2020.1\tps\win64\git-2.16.2\bin\bash.exe -c '/usr/bin/printenv PATH'")
+}
+
+query_vars_cygwin() {
+  (cmd.exe /c "call D:\Xilinx\Vitis\2020.1\settings64.bat && D:\cygwin64\bin\bash.exe -c '/usr/bin/printenv PATH'")
+}
+
+echo -e "\033[42;31mScript ver 0.1.002 \nMake sure the env path is correct before running!! \033[0m"
+
+#if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#    source /opt/Xilinx/Vitis/2020.1/settings64.sh
+#fi
+
+#case "$OSTYPE" in
+#solaris*) echo "SOLARIS" ;;
+#darwin*) echo "OSX" ;;
+#linux*) echo "LINUX" ;;
+#bsd*) echo "BSD" ;;
+#msys*) echo "WINDOWS" ;;
+#cygwin*) echo "ALSO WINDOWS" ;;
+#*) echo "unknown: $OSTYPE" ;;
+#esac
+
+#case "$OSTYPE" in
+#    solaris*) 
+#        echo "SOLARIS" 
+#        ;;
+#    darwin*) 
+#        echo "OSX" 
+#        ;;
+#    linux*) 
+#        echo "LINUX" 
+#        ;;
+#    bsd*) 
+#        echo "BSD" 
+#        ;;
+#    msys*) 
+#        echo "MSYS2" 
+#        ;;
+#    cygwin*) 
+#        echo "CYGWIN" 
+#        ;;
+#    *) 
+#        echo "unknown: $OSTYPE" 
+#        ;;
+#esac
+
+if [[ "$SETENV" -eq "" ]]; then
+    echo "SETENV has not been set before, setting ..."
+
+    case "$OSTYPE" in
+        linux*) 
+            echo "OSTYPE = $OSTYPE"
+            source /opt/Xilinx/Vitis/2020.1/settings64.sh
+            ;;
+        msys*) 
+            echo "OSTYPE = $OSTYPE"
+            export PATH="$PATH:$(query_vars_msys):/c/Users/dengl/AppData/Roaming/MobaXterm/slash/bin/"
+            if [ $? != 0 ]
+            then
+                echo -e "\n"
+                echo -e "\033[41;36m setting vars fail!!! \033[0m"
+                exit 1
+            fi
+            ;;
+        cygwin*) 
+            echo "OSTYPE = $OSTYPE"
+            export PATH="$PATH:$(query_vars_cygwin):/cygdrive/c/Users/dengl/AppData/Roaming/MobaXterm/slash/bin/"
+            if [ $? != 0 ]
+            then
+                echo -e "\n"
+                echo -e "\033[41;36m setting vars fail!!! \033[0m"
+                exit 1
+            fi
+            ;;
+        *) 
+            #echo "unknown: $OSTYPE" 
+            echo -e "\033[41;36m OSTYPE Unkown: $OSTYPE \033[0m"
+            exit 1
+            ;;
+    esac
+else
+    echo SETENV=$SETENV
 fi
+
 #xsct
 
--- vitis/setenv_bash.bat
+++ vitis/setenv_bash.bat
@@ -4,5 +4,6 @@ REM # Copyright (c) 1986-2022 Xilinx, Inc.  All rights reserved.    #
 REM #################################################################
 
 CALL D:\Xilinx\Vitis\2020.1\settings64.bat
-set PATH=D:\msys64\usr\bin;%PATH%
+set PATH=D:\msys64\usr\bin;%PATH%;C:\Users\dengl\AppData\Roaming\MobaXterm\slash\bin
+set SETENV=1
 start bash.exe
